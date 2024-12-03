8d0255e42b87d7ee511f3d3ca9df4339a66c7d41
NebulaKnight0503 <s.p.alpha0503@gmail.com>
2024-12-03 00:18:42 -0600

    Add support for numbered sample input files

--- helpers/args.ts
+++ helpers/args.ts
@@ -2,6 +2,18 @@ export function usingSampleData(): boolean {
   return ['-s', '--sample'].includes(Deno.args[0])
 }
 
+export function usingSampleDataNumber(): number|null {
+  return usingSampleData() && (Deno.args[1] || '').match(/^\d+$/) ? Number.parseInt(Deno.args[1], 10) : null
+}
+
 export function getInputFileName(): string {
-  return usingSampleData() ? 'sample.txt' : 'input.txt'
+  if (usingSampleData()) {
+    const sampleNumber = usingSampleDataNumber()
+    if (sampleNumber) {
+      return `sample${sampleNumber}.txt`
+    }
+    return 'sample.txt'
+  }
+
+  return 'input.txt'
 }
