ece8864fa9de9b069cdf5388f3fe7e3d7bc2f848
NebulaKnight0503 <s.p.alpha0503@gmail.com>
2024-12-09 09:56:27 -0600

    Day 09 solution, part 2

--- day09/main.ts
+++ day09/main.ts
@@ -6,6 +6,16 @@ import { getInputFileName } from '../helpers/args.ts'
 let answerPt1 = 0
 let answerPt2 = 0
 
+type FreeBlock = {
+  startIndex: number
+  size: number
+}
+type FileBlock = {
+  fileId: number
+  startIndex: number
+  size: number
+}
+
 // Get a readable stream from the input file doesn't have to be fully loaded into memory
 const rowReader = await getInputRowStream(getInputFileName(), {  delimiter: '' })
 
@@ -15,7 +25,10 @@ for await (const row of rowReader) {
 }
 
 const occurrenceCounts:Record<string, number> = {}
+const freeBlockGroups:FreeBlock[] = []
+const fileBlockGroups:FileBlock[] = []
 let countOfNonFreeBlocks = 0
+let mapIndex = 0
 const fileSystemMap:string[] = diskMap.reduce(
   (outputArr:string[], num:number, index:number) => {
     const isFile = index % 2 === 0
@@ -23,21 +36,27 @@ const fileSystemMap:string[] = diskMap.reduce(
       const fileId = Math.floor(index / 2)
       const fileSpace:string[] = Array(num).fill(fileId.toString())
       outputArr.push(...fileSpace)
+
       occurrenceCounts[fileId.toString()] = (occurrenceCounts[fileId.toString()] || 0) + num
       countOfNonFreeBlocks += num
+      fileBlockGroups.push({ fileId, startIndex: mapIndex, size: num })
     } else {
       const freeSpace:string[] = Array(num).fill('.')
       outputArr.push(...freeSpace)
+
       occurrenceCounts['.'] = (occurrenceCounts['.'] || 0) + num
+      freeBlockGroups.push({ startIndex: mapIndex, size: num })
     }
+    mapIndex += num
     return outputArr
   },
   []
 )
+const highestFileId = Number.parseInt(fileSystemMap[fileSystemMap.length - 1], 10)
 
 // console.debug('fileSystemMap:\n' + fileSystemMap.join(''))
 
-const compactedFileSystemMap:string[] = Array(fileSystemMap.length).fill('.')
+const compactedFileSystemMap1:string[] = Array(fileSystemMap.length).fill('.')
 
 let buildingIndex = 0
 let compactingIndex = fileSystemMap.length - 1
@@ -52,7 +71,7 @@ while (buildingIndex < countOfNonFreeBlocks) {
     currentEntry = fileSystemMap[compactingIndex--]
   }
 
-  compactedFileSystemMap[buildingIndex] = currentEntry
+  compactedFileSystemMap1[buildingIndex] = currentEntry
 
   // Compute checksum
   if (currentEntry !== '.') {
@@ -63,13 +82,77 @@ while (buildingIndex < countOfNonFreeBlocks) {
   buildingIndex++
 }
 
-// console.debug('compactedFileSystemMap:\n' + compactedFileSystemMap.join(''))
+// console.debug('compactedFileSystemMap1:\n' + compactedFileSystemMap1.join(''))
 
 // Validation
-for (const [char, expectedCount] of Object.entries(occurrenceCounts)) {
-  const actualCount = compactedFileSystemMap.filter(c => c === char).length
+for (const [entry, expectedCount] of Object.entries(occurrenceCounts)) {
+  const actualCount = compactedFileSystemMap1.filter(c => c === entry).length
   if (actualCount !== expectedCount) {
-    console.error(`Expected ${expectedCount} of "${char}", but found ${actualCount}`)
+    console.error(`Expected ${expectedCount} of "${entry}", but found ${actualCount}`)
+  }
+}
+
+// Part 2
+const compactedFileSystemMap2:string[] = fileSystemMap.slice()
+
+let buildingIndex2 = 0
+let buildingFileId = 0
+let compactingFileId = highestFileId
+
+// TODO: This check is probably wrong again....
+for (let i = fileBlockGroups.length - 1; i > 0; i--) {
+  const highestFile = fileBlockGroups[i]
+  const earlySufficientFreeSpace = freeBlockGroups.findIndex(({ startIndex, size }) => size >= highestFile.size && startIndex < highestFile.startIndex)
+
+  // If none big enough, leave in original spot
+  if (earlySufficientFreeSpace === -1) {
+    continue
+  }
+
+  const freeBlock = freeBlockGroups[earlySufficientFreeSpace]
+  const remainingFreeSpace = freeBlock.size - highestFile.size
+
+  // Compact the file into the free space
+  compactedFileSystemMap2.splice(freeBlock.startIndex, highestFile.size, ...Array(highestFile.size).fill(highestFile.fileId.toString()))
+  // Remove the file from its old space
+  compactedFileSystemMap2.splice(highestFile.startIndex, highestFile.size, ...Array(highestFile.size).fill('.'))
+
+  if (remainingFreeSpace === 0) {
+    freeBlockGroups.splice(earlySufficientFreeSpace, 1)
+  } else {
+    // Create the newly released free space
+    const nearestFreeBlockIndex = freeBlockGroups.findLastIndex(({ startIndex, size }) => startIndex < highestFile.startIndex)
+    const nearestFreeBlock = freeBlockGroups[nearestFreeBlockIndex]
+    // If the free block butts up against the highest file, we can just extend it
+    if (nearestFreeBlock.startIndex + nearestFreeBlock.size === highestFile.startIndex) {
+      nearestFreeBlock.size += highestFile.size
+    } else {
+      // Otherwise, we need to insert a new free block
+      const newFreeBlock:FreeBlock = { startIndex: highestFile.startIndex, size: highestFile.size }
+      freeBlockGroups.splice(nearestFreeBlockIndex + 1, 0, newFreeBlock)
+    }
+
+    freeBlock.startIndex += highestFile.size
+    freeBlock.size = remainingFreeSpace
+  }
+}
+
+// Compute checksum
+for (let i = 0; i < compactedFileSystemMap2.length; i++) {
+  const currentEntry = compactedFileSystemMap2[i]
+  if (currentEntry !== '.') {
+    //console.debug(`+= ${buildingIndex2} * ${Number.parseInt(currentEntry, 10)}`)
+    answerPt2 += i * Number.parseInt(currentEntry, 10)
+  }
+}
+
+//console.debug('compactedFileSystemMap2:\n' + compactedFileSystemMap2.join(''))
+
+// Validation
+for (const [entry, expectedCount] of Object.entries(occurrenceCounts)) {
+  const actualCount = compactedFileSystemMap2.filter(c => c === entry).length
+  if (actualCount !== expectedCount) {
+    console.error(`Expected ${expectedCount} of "${entry}", but found ${actualCount}`)
   }
 }
 
